eeff589d645f4205233a39085022f8813130d37b
Srajal Dwivedi <srajal72dwivedi@gmail.com>
2025-03-19 00:41:37 +0530

    Resume Link

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -3,13 +3,20 @@ import React from 'react'
 
 const CTA = () => {
   return (
-    <div className='cta'>
-        <a href="https://drive.google.com/drive/folders/1SMVUl456VWiLuQBvdWzA1vAnPVRifyTb?usp=sharing" className='btn' target='_blank'>Download CV</a>
-        {/* <a href={CV} download className='btn'>Download CV</a> */}
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    <div className="cta">
+      <a
+        href="https://drive.google.com/file/d/1i3zlPKo_mN-GyYpDDLFSGWNEwkBdASHt/view?usp=drive_link"
+        className="btn"
+        target="_blank"
+      >
+        Download CV
+      </a>
+      {/* <a href={CV} download className='btn'>Download CV</a> */}
+      <a href="#contact" className="btn btn-primary">
+        Let's Talk
+      </a>
     </div>
   )
 }
 
 export default CTA
-
